e6ec01e809caba3f5a2c6f47e57b63056fdfd310
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-05-06 21:29:26 +0600

    Added Problem 2: Reverse String

--- solution.ts
+++ solution.ts
@@ -3,4 +3,7 @@ const filterEvenNumbers = (arr: number[]): number[] => {
   return arr.filter((value: number) => value % 2 === 0);
 };
 
-
+// Problem 2: Reverse String function
+const reverseString = (str: string): string => {
+  return str.split("").reverse().join("");
+};
